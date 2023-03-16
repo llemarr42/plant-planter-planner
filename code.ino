3cadee808791851888d9e622476baa2ab7601acf
Loren Lemarr <56924009+llemarr42@users.noreply.github.com>
2023-03-16 14:18:40 -0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -4,48 +4,58 @@ Servo myServo2;
 int restAngle1 = 40;
 int moveAngle1 = 90;
 int Angle1 = 40;
-int restAngle2 = 94;
+int restAngle2 = 95;
 int moveAngle2 = 97;
-int undoAngle2 = 91;
+int undoAngle2 = 93;
 int i = 0;
-int button = 0;
+bool button = LOW;
+bool previous = LOW;
 int pause = 0;
-int buttonPin = 7;
+int buttonPin = 4;
 void setup() {
    Serial.begin(9600);
   myServo.attach(10);
   myServo2.attach(9);
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   myServo2.write(restAngle2);
   myServo.write(restAngle1);
 }
 void loop() {
-  for  (i ; i <100; i +=1){
-    Serial.println(Angle1);
-    myServo.write(Angle1);
-    delay(100);    
-    if (Angle1 <= moveAngle1) {
-      Angle1 += 1;
+  button = digitalRead(buttonPin);
+  Serial.println(button);
+  if (button == LOW && previous == HIGH){
+    for  (i ; i <100; i +=1){
+      Serial.println(Angle1);
+      myServo.write(Angle1);
+      delay(100);    
+      //Serial.println(button);
+
+      if (Angle1 <= moveAngle1) {
+        Angle1 += 1;
+      }
+      if (i <= 50) {
+        myServo2.write(moveAngle2);
+      }
+      else {
+        myServo2.write(restAngle2);
+      }
     }
-    if (i <= 50) {
-      myServo2.write(moveAngle2);
-    }
-    else {
-      myServo2.write(restAngle2);
-    }
+    for  (i ; i > 0; i -=1){
+      Serial.println(Angle1);
+      myServo.write(Angle1);
+      delay(100);    
+      //Serial.println(button);
+
+      if (Angle1 >= restAngle1) { 
+        Angle1-= 1;
+      }
+      if (i >= 50) {
+        myServo2.write(undoAngle2);
+      }
+      else {
+        myServo2.write(restAngle2);
+      }
+   }
   }
-  for  (i ; i > 0; i -=1){
-    Serial.println(Angle1);
-    myServo.write(Angle1);
-    delay(100);    
-    if (Angle1 >= restAngle1) { 
-      Angle1-= 1;
-    }
-    if (i >= 50) {
-      myServo2.write(undoAngle2);
-    }
-    else {
-      myServo2.write(restAngle2);
-    }
-  }
+  previous = button;
 }
