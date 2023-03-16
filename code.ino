723bb5692202966b7535421aefa8c65dd7c313b7
Loren Lemarr <56924009+llemarr42@users.noreply.github.com>
2023-03-16 14:29:26 -0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,15 +1,19 @@
 #include <Servo.h>
 Servo myServo;
 Servo myServo2;
+Servo myServo3;
 int restAngle1 = 40;
 int moveAngle1 = 90;
 int Angle1 = 40;
 int restAngle2 = 94.5;
 int moveAngle2 = 97;
 int undoAngle2 = 92;
+int restAngle3 = 40;
+int moveAngle3 = 90;
+int Angle3 = 40;
 int i = 0;
-bool button = LOW;
-bool previous = LOW;
+bool button = HIGH;
+bool previous = HIGH;
 int pause = 0;
 int buttonPin = 4;
 void setup() {
