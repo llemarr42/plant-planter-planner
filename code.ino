aa064a419633ee51a0d4802cbb9d45c9af7bd3f8
Loren Lemarr <56924009+llemarr42@users.noreply.github.com>
2023-04-10 14:44:03 -0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -5,12 +5,12 @@ Servo myServo3;
 int restAngle1 = 60;
 int moveAngle1 = 90;
 int Angle1 = 60;
-int restAngle2 = 94.5;
+int restAngle2 = 95;
 int moveAngle2 = 97;
-int undoAngle2 = 92;
-//int restAngle3 = 40;
-//int moveAngle3 = 90;
-//int Angle3 = 40;
+int undoAngle2 = 93;
+int restAngle3 = 80;
+int moveAngle3 = 160;
+int Angle3 = 80;
 int i = 0;
 bool button = HIGH;
 bool previous = HIGH;
@@ -22,53 +22,53 @@ void setup() {
   myServo2.attach(9);
   myServo3.attach(13);
 
-  pinMode(buttonPin, INPU T_PULLUP);
-  //myServo2.write(restAngle3);
+  pinMode(buttonPin, INPUT_PULLUP);
+  myServo3.write(restAngle3);
   myServo2.write(restAngle2);
   myServo.write(restAngle1);
 }
 void loop() {
   button = digitalRead(buttonPin);
-  Serial.println(button);
+  //Serial.println(button);
   if (button == LOW && previous == HIGH){
     for  (i ; i <100; i +=1){
       Serial.println(Angle1);
       myServo.write(Angle1);
-      //myServo.write(Angle3);
+      myServo.write(Angle3);
       delay(100);    
-      //Serial.println(button);
+      Serial.println(Angle1);
 
-      if (Angle1 <= moveAngle1) {
+      if (Angle1 < moveAngle1) {
         Angle1 += 1;
       }
-      if (i <= 50) {
+      if (i <= 20) {
         myServo2.write(moveAngle2);
       }
       else {
         myServo2.write(restAngle2);
       }
-      //if (Angle3 <= moveAngle3) {
-      //  Angle3 += 1;
-      //}
+      if (Angle3 <= moveAngle3) {
+        Angle3 += 1;
+      } 
     }
     for  (i ; i > 0; i -=1){
       Serial.println(Angle1);
       myServo.write(Angle1);
-     // myServo.write(Angle3);
+      myServo.write(Angle3);
       delay(100);    
-      //Serial.println(button);
+      Serial.println(Angle1);
       if (Angle1 >= restAngle1) { 
-        Angle1-= 1;
+        Angle1 -= 1;
       }
-      if (i >= 50) {
+      if (i >= 20) {
         myServo2.write(undoAngle2);
       }
       else {
         myServo2.write(restAngle2);
       }
-      //if (Angle3 >= restAngle3) { 
-       // Angle3-= 1;
-      //}
+      if (Angle3 > restAngle3) { 
+        Angle3 -= 1;
+      }
    }
   }
   previous = button;
