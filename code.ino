bb49cfab7ce2e1653c83b5d10c709e1a5dc3f5be
Loren Lemarr <56924009+llemarr42@users.noreply.github.com>
2023-04-20 14:56:59 -0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,8 +1,17 @@
+//import the servo code
+
 #include <Servo.h>
+
+//naming the servos
+
 Servo myServo;
 Servo myServo2;
 Servo myServo3;
 Servo myServo4;
+Servo myServo5;
+
+//creates/defines the angles that each joint of the arm/claw move to/from
+
 int restAngle1 = 90;
 int moveAngle1 = 130;
 int Angle1 = 90;
@@ -15,28 +24,40 @@ int Angle3 = 120;
 int restAngle4 = 90;
 int moveAngle4 = 45;
 int Angle4 = 90;
+int restAngle5 = 90;
+int moveAngle5 = 45;
+int Angle5 = 90;
+
+//sets up the for loop variables as well as the button variables
+
 int i = 0;
 bool button = HIGH;
 bool previous = HIGH;
 int pause = 0;
 int buttonPin = 4;
+
 void setup() {
-   Serial.begin(9600);
+  
+   Serial.begin(9600); // sets up the serial moniter
+   pinMode(buttonPin, INPUT_PULLUP); //changes the pinmode for the button pin to input and pullup which makes the pin rest at HIGH and when the circuit is complete it changes to LOW 
+
+  //atatches the servos to pin on the arduino uno
+
   myServo.attach(10);
   myServo2.attach(9);
   myServo3.attach(13);
   myServo4.attach(12);
 
-  pinMode(buttonPin, INPUT_PULLUP);
+  //moves the servos to there rest position
+
   myServo4.write(restAngle4);
   myServo3.write(restAngle3);
   myServo2.write(restAngle2);
   myServo.write(restAngle1);
 }
 void loop() {
-  button = digitalRead(buttonPin);
-  //Serial.println(button);
-  if (button == LOW && previous == HIGH){
+  button = digitalRead(buttonPin); //button variable changes to the imput on the buttonpin
+  if (button == LOW && previous == HIGH){ //if the button is pressed then the code runs
     for  (i ; i <100; i +=1){
       Serial.println(Angle3);
       myServo.write(Angle1);
@@ -82,5 +103,5 @@ void loop() {
       }
    }
   }
-  previous = button;
+  previous = button; //ensures that the code doesn't break if the button is held
 }
